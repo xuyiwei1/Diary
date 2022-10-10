da6d0a7476a1361769211bf5224770124910d071
yiweixu <511878701@qq.com>
2022-10-10 15:54:30 +0800

    添加HomePage的DatePicker按钮

--- app/src/main/java/comp5216/sydney/edu/au/diarypro/HomeActivity.java
+++ app/src/main/java/comp5216/sydney/edu/au/diarypro/HomeActivity.java
@@ -1,12 +1,18 @@
 package comp5216.sydney.edu.au.diarypro;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
+
+import java.util.Calendar;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -30,5 +36,33 @@ public class HomeActivity extends AppCompatActivity {
         });
     }
 
+    /**
+     * when user click the button,show date picker
+     * @param view
+     */
+    public void showDatePicker(View view) {
+        DatePickerFragment datePickerFragment = new DatePickerFragment();
+        datePickerFragment.show(getSupportFragmentManager(), "datePicker");
+    }
+
+    public static class DatePickerFragment extends DialogFragment
+            implements DatePickerDialog.OnDateSetListener {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current date as the default date in the picker
+            final Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+
+            // Create a new instance of DatePickerDialog and return it
+            return new DatePickerDialog(getActivity(), this, year, month, day);
+        }
+
+        public void onDateSet(DatePicker view, int year, int month, int day) {
+            // Do something with the date chosen by the user
+        }
+    }
 
 }
