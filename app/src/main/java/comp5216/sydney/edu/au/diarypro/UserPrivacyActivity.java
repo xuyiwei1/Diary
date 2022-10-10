d89043212ea4c57203a8a527dec24e49c8d7146a
yiweixu <511878701@qq.com>
2022-10-10 09:52:23 +0800

    完善用户隐私条款的滑动

--- app/src/main/java/comp5216/sydney/edu/au/diarypro/UserPrivacyActivity.java
+++ app/src/main/java/comp5216/sydney/edu/au/diarypro/UserPrivacyActivity.java
@@ -1,14 +1,21 @@
 package comp5216.sydney.edu.au.diarypro;
 
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class UserPrivacyActivity extends AppCompatActivity {
+
+    private TextView privacyTextView;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_privacy);
+        privacyTextView = this.findViewById(R.id.privacyTextView);
+        privacyTextView.setMovementMethod(ScrollingMovementMethod.getInstance());
     }
 }
