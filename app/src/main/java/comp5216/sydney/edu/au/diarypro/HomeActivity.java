35bce8f30bc0a28d4257b1bb7a12e129b923d047
yiweixu <511878701@qq.com>
2022-10-13 10:04:39 +0800

    添加HomePage的长按删除功能

--- app/src/main/java/comp5216/sydney/edu/au/diarypro/HomeActivity.java
+++ app/src/main/java/comp5216/sydney/edu/au/diarypro/HomeActivity.java
@@ -1,10 +1,14 @@
 package comp5216.sydney.edu.au.diarypro;
 
+import android.app.AlertDialog;
 import android.app.DatePickerDialog;
 import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.ListView;
@@ -24,6 +28,7 @@ import comp5216.sydney.edu.au.diarypro.entity.DiaryItem;
 
 public class HomeActivity extends AppCompatActivity {
 
+    private static final String TAG = "HomeActivity";
     private Button settingBtn;
     private ListView diaryItemListView;
     private List<DiaryItem> diaryItems;
@@ -43,7 +48,7 @@ public class HomeActivity extends AppCompatActivity {
         //TODO query the database to get the diary items
         appDatabase = AppDatabase.getDatabase(this.getApplication().getApplicationContext());
         diaryItemDao = appDatabase.diaryItemDao();
-       // diaryItemDao.insertItem(new DiaryItem("work","Aug 24"));
+        //diaryItemDao.insertItem(new DiaryItem("study","Otc 24"));
         diaryItems = diaryItemDao.getAll();
        /* diaryItems = new ArrayList<>();
         diaryItems.add(new DiaryItem(1,"work","Aug 23"));
@@ -57,7 +62,7 @@ public class HomeActivity extends AppCompatActivity {
         diaryItemListView.setAdapter(diaryItemListViewAdapter);
 
         //TODO invoke the long click and click action on list view
-        //setupListViewListener();
+        setupListViewListener();
 
         //set the setting button click listener
         settingBtn.setOnClickListener(new View.OnClickListener() {
@@ -98,5 +103,73 @@ public class HomeActivity extends AppCompatActivity {
             // Do something with the date chosen by the user
         }
     }
+
+    /**
+     * handle the long click and click events
+     */
+    public void setupListViewListener() {
+        this.diaryItemListView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.i(TAG, "onItemLongClick: position:" + position + "row:" + id);
+                //create a alert dialog
+                AlertDialog.Builder builder = new AlertDialog.Builder(HomeActivity.this);
+                builder.setTitle(R.string.dialog_delete_title)
+                        .setMessage(R.string.dialog_delete_shop_item_msg)
+                        .setPositiveButton("delete", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                //remove the data from local database
+                                diaryItemDao.deleteById(diaryItems.get(position).getId());
+                                //when click the delete button delete the item in the listview
+                                diaryItems.remove(position);
+                                diaryItemListViewAdapter.notifyDataSetChanged();
+                            }
+                        })
+                        .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                // users click the cancel button, nothing happened
+                            }
+                        });
+                //show the alertDialog
+                builder.create().show();
+                return true;
+            }
+        });
+
+        //TODO handle the single click event
+        /*this.diaryItemListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                DiaryItem shopItem = (DiaryItem) diaryItemListViewAdapter.getItem(position);
+                Log.d(TAG, "onItemClick: position" + position + " getItem: " + shopItem);
+                // switch to the edit add new item layout
+                Intent intent = new Intent(MainActivity.this, EditAddItemActivity.class);
+                if (intent != null) {
+                    intent.putExtra("itemName", shopItem.getName());
+                    intent.putExtra("itemNumber", shopItem.getNumber());
+                    intent.putExtra("isChecked", shopItem.isChecked());
+                    intent.putExtra("position", position);
+
+                    //get the original id of the item to update in later, because of the delete action
+                    ShopItemDao shopItemDao = new ShopItemDao(MainActivity.this);
+                    List<ShopItem> shopItemList = shopItemDao.queryAll();
+                    ShopItem originItem = null;
+                    for (ShopItem item : shopItemList) {
+                        if (item.getName().equals(shopItem.getName())) {
+                            originItem = item;
+                        }
+                    }
+                    intent.putExtra("id", originItem.getId());
+
+                    mLauncher.launch(intent);
+                    itemAdapter.notifyDataSetChanged();
+                }
+            }
+        });*/
+    }
+
+
 
 }
