fc86f1e08cdbfb5f681ed5b0ec7c442aec068c42
yiweixu <511878701@qq.com>
2022-10-16 09:22:15 +0800

    完成work、study、event的页面构建，并处理部分交互

--- app/src/main/java/comp5216/sydney/edu/au/diarypro/AddItemsActivity.java
+++ app/src/main/java/comp5216/sydney/edu/au/diarypro/AddItemsActivity.java
@@ -1,6 +1,8 @@
 package comp5216.sydney.edu.au.diarypro;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,5 +12,32 @@ public class AddItemsActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_items);
+    }
+
+    /**
+     * click button jump to work edit page
+     * @param view
+     */
+    public void jumpToWorkEditPage(View view) {
+        Intent intent = new Intent(AddItemsActivity.this,WorkEditActivity.class);
+        startActivity(intent);
+    }
+
+    /**
+     * click button jump to study edit page
+     * @param view
+     */
+    public void jumpToStudyEditPage(View view) {
+        Intent intent = new Intent(AddItemsActivity.this,StudyEditActivity.class);
+        startActivity(intent);
+    }
+
+    /**
+     * click button jump to study edit page
+     * @param view
+     */
+    public void jumpToEventEditPage(View view) {
+        Intent intent = new Intent(AddItemsActivity.this,EventEditActivity.class);
+        startActivity(intent);
     }
 }
