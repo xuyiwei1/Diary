0d35428a188da617ee0acb2038a41b3663ddc5e1
yiweixu <511878701@qq.com>
2022-10-16 09:49:50 +0800

    完成work、study、event的页面构建，并处理部分按键交互

--- app/src/main/java/comp5216/sydney/edu/au/diarypro/EventEditActivity.java
+++ app/src/main/java/comp5216/sydney/edu/au/diarypro/EventEditActivity.java
@@ -1,15 +1,48 @@
 package comp5216.sydney.edu.au.diarypro;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class EventEditActivity extends AppCompatActivity {
 
+    private EditText eventEditText;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_edit);
+        eventEditText = this.findViewById(R.id.eventEditView);
+    }
+
+    /**
+     * when user click the cancel, cancel to save diary item
+     * @param view
+     */
+    public void cancelEditBtn(View view) {
+        //check whether user want to cancel to save info
+        AlertDialog.Builder builder = new AlertDialog.Builder(EventEditActivity.this);
+        builder.setTitle(R.string.dialog_cancel_title)
+                .setMessage(R.string.dialog_cancel_edit_add_item_msg)
+                .setPositiveButton("yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // discard the data and jump to the main page
+                        eventEditText.setText("");
+                        finish();
+                    }
+                })
+                .setNegativeButton("no", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // users click cancel button, noting to do
+                    }
+                });
+        builder.create().show();
     }
 }
--- app/src/main/java/comp5216/sydney/edu/au/diarypro/StudyEditActivity.java
+++ app/src/main/java/comp5216/sydney/edu/au/diarypro/StudyEditActivity.java
@@ -1,15 +1,48 @@
 package comp5216.sydney.edu.au.diarypro;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class StudyEditActivity extends AppCompatActivity {
 
+    private EditText studyEditText;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_study_edit);
+        studyEditText = this.findViewById(R.id.studyEditView);
+    }
+
+    /**
+     * when user click the cancel, cancel to save diary item
+     * @param view
+     */
+    public void cancelEditBtn(View view) {
+        //check whether user want to cancel to save info
+        AlertDialog.Builder builder = new AlertDialog.Builder(StudyEditActivity.this);
+        builder.setTitle(R.string.dialog_cancel_title)
+                .setMessage(R.string.dialog_cancel_edit_add_item_msg)
+                .setPositiveButton("yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // discard the data and jump to the main page
+                        studyEditText.setText("");
+                        finish();
+                    }
+                })
+                .setNegativeButton("no", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // users click cancel button, noting to do
+                    }
+                });
+        builder.create().show();
     }
 }
